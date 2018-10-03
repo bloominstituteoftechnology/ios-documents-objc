8eeb29a4cc451d935d0a80259cb54df1b5909217
Conner Alegre <cga0@pm.me>
2018-10-03 14:17:17 -0600

    add minor styling and vc for document detail view

---
+++ ios-documents-objc/ios-documents-objc/View Controllers/DocumentDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DocumentDetailViewController.h
+//  ios-documents-objc
+//
+//  Created by Conner on 10/3/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DocumentDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
