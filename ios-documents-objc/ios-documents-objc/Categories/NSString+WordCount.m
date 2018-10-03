a54391b3562dff414093b080b16541ddec49cf76
Conner Alegre <cga0@pm.me>
2018-10-03 14:22:18 -0600

    add blank word count category (extension) on NSString

---
+++ ios-documents-objc/ios-documents-objc/Categories/NSString+WordCount.m
@@ -0,0 +1,13 @@
+//
+//  NSString+WordCount.m
+//  ios-documents-objc
+//
+//  Created by Conner on 10/3/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import "NSString+WordCount.h"
+
+@implementation NSString (WordCount)
+
+@end
