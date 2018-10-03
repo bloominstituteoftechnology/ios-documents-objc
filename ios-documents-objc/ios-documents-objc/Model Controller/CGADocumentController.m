a6941075a34b877929cd9023c3248b599531aacf
Conner Alegre <cga0@pm.me>
2018-10-03 16:07:59 -0600

    implement create, update, delete into document controller

--- ios-documents-objc/ios-documents-objc/Model Controller/CGADocumentController.m
+++ ios-documents-objc/ios-documents-objc/Model Controller/CGADocumentController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CGADocumentController.h"
+#import "CGADocument.h"
 
 @implementation CGADocumentController
 
@@ -17,6 +18,26 @@
         _documents = [[NSMutableArray alloc] init];
     }
     return self;
+}
+
+- (void)createDocumentWithTitle:(NSString *)title text:(NSString *)text {
+    CGADocument *document = [[CGADocument alloc] initWithTitle:title text:text];
+    [[self documents] addObject:document];
+}
+
+- (void)updateDocument:(CGADocument *)document title:(NSString *)title text:(NSString *)text {
+    CGADocument *tempDoc;
+    tempDoc.title = title;
+    tempDoc.text = text;
+    
+    
+    NSUInteger index = [[self documents] indexOfObject:document];
+    [[self documents] replaceObjectAtIndex:index withObject:tempDoc];
+}
+
+- (void)deleteDocument:(CGADocument *)document {
+    NSUInteger index = [[self documents] indexOfObject:document];
+    [[self documents] removeObjectAtIndex:index];
 }
 
 @end
