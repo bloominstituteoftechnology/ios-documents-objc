ca774ef2bf9a5877a6c96f85da58ec233c17dfb7
Conner Alegre <cga0@pm.me>
2018-10-03 14:55:20 -0600

    add blank model for document

---
+++ ios-documents-objc/ios-documents-objc/Model/CGADocument.h
@@ -0,0 +1,17 @@
+//
+//  CGADocument.h
+//  ios-documents-objc
+//
+//  Created by Conner on 10/3/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CGADocument : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-documents-objc/ios-documents-objc/Model/CGADocument.m
@@ -0,0 +1,13 @@
+//
+//  CGADocument.m
+//  ios-documents-objc
+//
+//  Created by Conner on 10/3/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import "CGADocument.h"
+
+@implementation CGADocument
+
+@end
