dc9be1af38b90b3b506b6e8820750a97323eebc9
Conner Alegre <cga0@pm.me>
2018-10-03 16:31:13 -0600

    add documentController and document to detail view

--- ios-documents-objc/ios-documents-objc/View Controllers/DocumentDetailViewController.h
+++ ios-documents-objc/ios-documents-objc/View Controllers/DocumentDetailViewController.h
@@ -7,10 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CGADocumentController.h"
+#import "CGADocument.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DocumentDetailViewController : UIViewController
+
+@property CGADocumentController *documentController;
+@property CGADocument *document;
 
 @end
 
