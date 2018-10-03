bb22354cd64d55c35b9a7686dd959c0d4d2bc27e
Linh Bouniol <paulalinh89@gmail.com>
2018-10-03 13:08:16 -0700

    Initial commit

---
+++ Documents/Documents/App Lifecycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Documents
+//
+//  Created by Linh Bouniol on 10/3/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
