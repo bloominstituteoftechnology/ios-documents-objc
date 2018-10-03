65610ec0b27e18609ec390ad280d909531e2b149
Conner Alegre <cga0@pm.me>
2018-10-03 15:38:20 -0600

    add document model controller with empty documents property

---
+++ ios-documents-objc/ios-documents-objc/Model Controller/CGADocumentController.h
@@ -0,0 +1,17 @@
+//
+//  CGADocumentController.h
+//  ios-documents-objc
+//
+//  Created by Conner on 10/3/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CGADocumentController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-documents-objc/ios-documents-objc/Model Controller/CGADocumentController.m
@@ -0,0 +1,15 @@
+//
+//  CGADocumentController.m
+//  ios-documents-objc
+//
+//  Created by Conner on 10/3/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import "CGADocumentController.h"
+
+@implementation CGADocumentController
+
+@property NSMutableArray *documents;
+
+@end
