09f1e99939af332eeadd724da6b9c421399fea1e
Dillon McElhinney <dillon.mce@gmail.com>
2019-02-27 13:19:22 -0700

    Initial commit
    Make empty Xcode project.

---
+++ Documents-Objc/Documents-Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Documents-Objc
+//
+//  Created by Dillon McElhinney on 2/27/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Documents-Objc/Documents-ObjcTests/Documents_ObjcTests.m
@@ -0,0 +1,37 @@
+//
+//  Documents_ObjcTests.m
+//  Documents-ObjcTests
+//
+//  Created by Dillon McElhinney on 2/27/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Documents_ObjcTests : XCTestCase
+
+@end
+
+@implementation Documents_ObjcTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
