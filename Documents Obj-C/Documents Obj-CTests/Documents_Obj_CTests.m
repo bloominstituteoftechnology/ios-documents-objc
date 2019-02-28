78a108c1d5d4b8f2a4033650371b7172c15a1a45
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-02-27 19:05:48 -0800

    Initial Commit

---
+++ Documents Obj-C/Documents Obj-CTests/Documents_Obj_CTests.m
@@ -0,0 +1,37 @@
+//
+//  Documents_Obj_CTests.m
+//  Documents Obj-CTests
+//
+//  Created by Ivan Caldwell on 2/27/19.
+//  Copyright © 2019 Ivan Caldwell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Documents_Obj_CTests : XCTestCase
+
+@end
+
+@implementation Documents_Obj_CTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
