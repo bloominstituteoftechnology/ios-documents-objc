2e64a0d061ba1fa1ededde329136aadb701dcd33
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-27 23:30:55 -0700

    storyboard set up

---
+++ Documents/Documents/View Controllers/MJRDocumentDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  MJRDocumentDetailViewController.h
+//  Documents
+//
+//  Created by Moses Robinson on 3/27/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJRDocumentDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Documents/Documents/View Controllers/MJRDocumentDetailViewController.m
@@ -0,0 +1,29 @@
+//
+//  MJRDocumentDetailViewController.m
+//  Documents
+//
+//  Created by Moses Robinson on 3/27/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import "MJRDocumentDetailViewController.h"
+
+@interface MJRDocumentDetailViewController ()
+
+@property (strong, nonatomic) IBOutlet UILabel *wordCountLabel;
+@property (strong, nonatomic) IBOutlet UITextField *titleTextField;
+@property (strong, nonatomic) IBOutlet UITextView *documentTextView;
+
+@end
+
+@implementation MJRDocumentDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+
+@end
