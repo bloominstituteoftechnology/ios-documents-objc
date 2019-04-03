fa8d93945acf5918691bf6be613a6eb64840af01
Paul Yi <yipaulc@gmail.com>
2019-04-03 15:06:55 -0700

    Create NSString category WordCount

---
+++ DocumentsObj-C/DocumentsObj-C/NSString+PCYWordCount.m
@@ -0,0 +1,18 @@
+//
+//  NSString+PCYWordCount.m
+//  DocumentsObj-C
+//
+//  Created by Paul Yi on 4/3/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+#import "NSString+PCYWordCount.h"
+
+@implementation NSString (PCYWordCount)
+
+- (int)wordCount {
+    NSArray *words = [self componentsSeparatedByString:@" "];
+    return words.count;
+}
+
+@end
