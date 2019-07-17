c36d1d93ac2bc6a51bbfb141c6360efe29f0fb3c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-17 14:06:33 -0700

    init

---
+++ documents-obj/documents-obj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  documents-obj
+//
+//  Created by Hector Steven on 7/17/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ documents-obj/documents-obj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  documents-obj
+//
+//  Created by Hector Steven on 7/17/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
