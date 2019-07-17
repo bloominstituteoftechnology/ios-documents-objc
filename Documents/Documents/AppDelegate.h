6f8b543a2e9e838e25e27a5a16b11e00daf0386b
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-17 11:11:06 -0600

    Initial commit with new Xcode project

---
+++ Documents/Documents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Documents
+//
+//  Created by Mitchell Budge on 7/17/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
