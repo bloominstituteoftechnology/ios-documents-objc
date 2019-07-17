0e9692637a382483f24bab934637d1cf8d75662a
John Pitts <jgpitts@gmail.com>
2019-07-17 16:08:44 -0400

    Completed storyboard with TableView + AddDocumentVC including save button title text field, wordCount label, and body objects. Table includes an add button and Subtitle cell

---
+++ DocumentsObjC1Wed/DocumentsObjC1Wed/LSIDocument.h
@@ -0,0 +1,19 @@
+//
+//  LSIDocument.h
+//  DocumentsObjC1Wed
+//
+//  Created by John Pitts on 7/17/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIDocument : NSObject
+
+@property NSString]
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DocumentsObjC1Wed/DocumentsObjC1Wed/LSIDocument.m
@@ -0,0 +1,13 @@
+//
+//  LSIDocument.m
+//  DocumentsObjC1Wed
+//
+//  Created by John Pitts on 7/17/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+#import "LSIDocument.h"
+
+@implementation LSIDocument
+
+@end
