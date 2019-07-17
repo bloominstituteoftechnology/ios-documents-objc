b214dbd0639322dc4bb59c71121d2082c798ad37
John Pitts <jgpitts@gmail.com>
2019-07-17 18:26:27 -0400

    created Category to calculate word count in NSString so we can count the words in the TextView.  Not satisfied with the array syntax used, double-check it even if it works.

---
+++ DocumentsObjC1Wed/DocumentsObjC1Wed/MODEL/NSString+WordCount.h
@@ -0,0 +1,19 @@
+//
+//  NSString+WordCount.h
+//  DocumentsObjC1Wed
+//
+//  Created by John Pitts on 7/17/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (WordCount)
+
+- (int *)wordCount;
+
+@end
+
+NS_ASSUME_NONNULL_END
