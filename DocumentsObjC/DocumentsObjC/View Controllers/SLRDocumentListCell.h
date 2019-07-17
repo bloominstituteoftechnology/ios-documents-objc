159d76a2950b8ac206281529d2f4bf4057c900e8
Sameera Leola <pixelandbyte@gmail.com>
2019-07-17 16:28:17 -0500

    Initial commit. Created project and added views, view objects, and controllers

---
+++ DocumentsObjC/DocumentsObjC/View Controllers/SLRDocumentListCell.h
@@ -0,0 +1,17 @@
+//
+//  SLRDocumentListCell.h
+//  DocumentsObjC
+//
+//  Created by Sameera Roussi on 7/17/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLRDocumentListCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DocumentsObjC/DocumentsObjC/View Controllers/SLRDocumentListCell.m
@@ -0,0 +1,24 @@
+//
+//  SLRDocumentListCell.m
+//  DocumentsObjC
+//
+//  Created by Sameera Roussi on 7/17/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+#import "SLRDocumentListCell.h"
+
+@implementation SLRDocumentListCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
