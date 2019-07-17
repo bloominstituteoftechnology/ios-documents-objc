777bd4187d654a0613d1a243b76caa3b217f4f63
John Pitts <jgpitts@gmail.com>
2019-07-17 16:29:34 -0400

    completed Storyboard by adding all outlets to appropriate files, connecting VC files to Storyboard, & added Storyboard titles just in case needed for UITesting

---
+++ DocumentsObjC1Wed/DocumentsObjC1Wed/LSIDetailAddDocumentViewController.m
@@ -0,0 +1,33 @@
+//
+//  LSIDetailAddDocumentViewController.m
+//  DocumentsObjC1Wed
+//
+//  Created by John Pitts on 7/17/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+#import "LSIDetailAddDocumentViewController.h"
+
+@interface LSIDetailAddDocumentViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UITextView *titleTextView;
+@property (weak, nonatomic) IBOutlet UILabel *wordCountLabel;
+
+@end
+
+@implementation LSIDetailAddDocumentViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)saveButtonTapped:(id)sender {
+    
+}
+
+
+
+@end
