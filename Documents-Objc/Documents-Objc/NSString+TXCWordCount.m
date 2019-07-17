ed32b11952a9e7b75a0ddd4b411cc58d6d872904
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-07-17 16:42:35 -0400

    Added a category for word count

---
+++ Documents-Objc/Documents-Objc/NSString+TXCWordCount.m
@@ -0,0 +1,24 @@
+//
+//  NSString+TXCWordCount.m
+//  Documents-Objc
+//
+//  Created by Thomas Cacciatore on 7/17/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+#import "NSString+TXCWordCount.h"
+
+@implementation NSString (TXCWordCount)
+
+- (NSInteger)wordCount {
+    NSCharacterSet *separators = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    NSArray *words = [self componentsSeparatedByCharactersInSet:separators];
+    
+    NSIndexSet *separatorIndexes = [words indexesOfObjectsPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        return [obj isEqualToString:@""];
+    }];
+    
+    return [words count] - [separatorIndexes count];
+}
+
+@end
