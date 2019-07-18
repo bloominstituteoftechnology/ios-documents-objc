7f16aad6b16d491ffc5f23031c17a1648a085bee
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-17 20:40:19 -0700

    models start

---
+++ documents-obj/documents-obj/Models/HSVDocument.h
@@ -0,0 +1,17 @@
+//
+//  HSVDocument.h
+//  documents-obj
+//
+//  Created by Hector Steven on 7/17/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSVDocument : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ documents-obj/documents-obj/Models/HSVDocument.m
@@ -0,0 +1,13 @@
+//
+//  HSVDocument.m
+//  documents-obj
+//
+//  Created by Hector Steven on 7/17/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import "HSVDocument.h"
+
+@implementation HSVDocument
+
+@end
