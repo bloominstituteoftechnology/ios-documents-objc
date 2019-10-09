22ffa05f41ce36acae5443e7057add4f3779117f
Michael Redig <mredig@gmail.com>
2019-10-09 16:08:14 -0500

    added rep document model

---
+++ ObjC Documents/ObjC Documents/Models/REPDocument.h
@@ -0,0 +1,17 @@
+//
+//  REPDocument.h
+//  ObjC Documents
+//
+//  Created by Michael Redig on 10/9/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface REPDocument : NSObject
+
+@property NSString* title;
+@property NSString* text;
+@property (readonly) NSUInteger wordCount;
+
+@end
