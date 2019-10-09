41a38a990befd77dd173a98ada95b99c635737c7
bradleyyin <bradleyyin@gmail.com>
2019-10-09 15:41:43 -0700

    imeplement detail view controller

--- Document-objc/Document-objc/DocumentDetailScreen/BYDocumentDetailViewController.m
+++ Document-objc/Document-objc/DocumentDetailScreen/BYDocumentDetailViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "BYDocumentDetailViewController.h"
+#import "BYDocument.h"
+#import "BYDocumentController.h"
+#import "NSString+WordCount.h"
 
-@interface BYDocumentDetailViewController ()
+@interface BYDocumentDetailViewController () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *wordCountLabel;
 @property (weak, nonatomic) IBOutlet UITextField *titleTextField;
 @property (weak, nonatomic) IBOutlet UITextView *bodyTextView;
@@ -20,8 +23,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    _bodyTextView.delegate = self;
+    [self updateViews];
 }
+
+- (void)saveTask {
+        BOOL isNewTask = self.document == nil;
+        
+    //    LSITask *task = self.task ? self.task : [[LSITask alloc] init];
+        BYDocument *document = self.document ?: [[BYDocument alloc] init];
+        
+        document.title = self.titleTextField.text;
+        document.bodyText = self.bodyTextView.text;
+        
+        if (isNewTask) {
+            [self.documentController createDocument:document];
+        }
+        
+        [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)updateViews {
+    if (!self.isViewLoaded || !self.document) { return; }
+    
+    self.title = self.document.title;
+    self.titleTextField.text = self.document.title;
+    self.bodyTextView.text = self.document.bodyText;
+}
+
 - (IBAction)saveDocument:(id)sender {
+    [self saveTask];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+    self.wordCountLabel.text = [NSString stringWithFormat:@"Word count: %d", [textView.text wordCount]];
 }
 
 
