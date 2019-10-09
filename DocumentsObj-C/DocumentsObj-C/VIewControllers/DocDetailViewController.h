b9f113b502b5dfa1882f419c2f6ee34c79da4303
luqmaan <luq.j.khan@gmail.com>
2019-10-09 16:42:03 -0500

    add sublass of view controller for doc detail vc

---
+++ DocumentsObj-C/DocumentsObj-C/VIewControllers/DocDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DocDetailViewController.h
+//  DocumentsObj-C
+//
+//  Created by Luqmaan Khan on 10/9/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DocDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
