d7bf596eba6e9652eca0e42b2887c13ededbe805
luqmaan <luq.j.khan@gmail.com>
2019-10-09 16:35:32 -0500

    add navigation controller and table view controller in IB

---
+++ DocumentsObj-C/DocumentsObj-C/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DocumentsObj-C
+//
+//  Created by Luqmaan Khan on 10/9/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
