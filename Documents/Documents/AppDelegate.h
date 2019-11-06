5595a8c15b0f2d78baeca4f35e4838a29cc32eb2
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-11-05 21:11:56 -0500

    Added Project

---
+++ Documents/Documents/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Documents
+//
+//  Created by Michael Stoffer on 11/5/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Documents/Documents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Documents
+//
+//  Created by Michael Stoffer on 11/5/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
