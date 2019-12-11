6aaccafe1495e28049630711830c5dcce76b1ab6
Isvvc <lyons@tuta.io>
2019-12-11 14:40:05 -0700

    Create Document Controller

---
+++ Documents/Documents/LSIDocumentController.m
@@ -0,0 +1,22 @@
+//
+//  LSIDocumentController.m
+//  Documents
+//
+//  Created by Isaac Lyons on 12/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import "LSIDocumentController.h"
+
+@implementation LSIDocumentController
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _documents = @[];
+    }
+    return self;
+}
+
+@end
