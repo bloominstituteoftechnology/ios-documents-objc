6808cc22f5bbecf233eafbced4279d99036af954
Gi Pyo Kim <gipyok@gmail.com>
2019-12-11 17:45:13 -0800

    Implemented the table view

--- Documents-Objc/Documents-Objc/View Controllers/GIPTableViewController.m
+++ Documents-Objc/Documents-Objc/View Controllers/GIPTableViewController.m
@@ -7,11 +7,15 @@
 //
 
 #import "GIPTableViewController.h"
+#import "GIPDocument.h"
+#import "GIPDocumentController.h"
+#import "GIPNewDocumentViewController.h"
 
 @interface GIPTableViewController ()
 
 - (IBAction)AddButtonPressed:(id)sender;
 
+@property GIPDocumentController *controller;
 
 @end
 
@@ -19,81 +23,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _controller = [[GIPDocumentController alloc] init];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    [self.tableView reloadData];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self.tableView reloadData];
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.controller.documents.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DocumentCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    cell.textLabel.text = [[self.controller.documents objectAtIndex:indexPath.row] title];
+    int wordCount = [[self.controller.documents objectAtIndex:indexPath.row] wordCount];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d words", wordCount];
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+        [self.controller removeDocument:[self.controller.documents objectAtIndex:indexPath.row]];
+        [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationLeft];
+    }
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier  isEqual: @"AddNewDocumentSegue"]) {
+        GIPNewDocumentViewController *newDocVC = [segue destinationViewController];
+        newDocVC.controller = self.controller;
+    }
 }
-*/
+
 
 - (IBAction)AddButtonPressed:(id)sender {
-    
+    [self performSegueWithIdentifier:@"AddNewDocumentSegue" sender:self];
 }
 @end
