3a48272319086b5287875cd130311e0a51da985b
Jon Bash <jonbash@protonmail.com>
2020-01-22 12:32:57 -0800

    init xcode proj

---
+++ Documents/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Documents
+//
+//  Created by Jon Bash on 2020-01-22.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
