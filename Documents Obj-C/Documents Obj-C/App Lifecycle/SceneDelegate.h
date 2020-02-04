1d8ec275a4d9a788dda3fef631a4051033f78c98
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-04 18:59:39 -0500

    organizing the files

---
+++ Documents Obj-C/Documents Obj-C/App Lifecycle/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Documents Obj-C
+//
+//  Created by Vici Shaweddy on 2/4/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
