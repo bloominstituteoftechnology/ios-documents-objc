fc714e5e215435138afd6656c45d86ab0278ebce
John Kouris <jdkouris@gmail.com>
2020-02-04 19:14:36 -0800

    Added the document controller with its properties and methods

---
+++ Documents/Documents/DocumentController.m
@@ -0,0 +1,13 @@
+//
+//  DocumentController.m
+//  Documents
+//
+//  Created by John Kouris on 2/4/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import "DocumentController.h"
+
+@implementation DocumentController
+
+@end
