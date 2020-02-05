e479d6f1acb91bc37e3500b78410bdffa9f8f2c8
John Kouris <jdkouris@gmail.com>
2020-02-04 19:25:28 -0800

    Implemented the document controller methods

--- Documents/Documents/DocumentController.m
+++ Documents/Documents/DocumentController.m
@@ -7,7 +7,42 @@
 //
 
 #import "DocumentController.h"
+#import "JDKDocument.h"
+
+@interface DocumentController ()
+
+@property (nonatomic) NSMutableArray<JDKDocument *> *internalDocuments;
+
+@end
 
 @implementation DocumentController
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _internalDocuments = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (void)createDocument:(JDKDocument *)aDocument
+{
+    [_internalDocuments addObject:aDocument];
+}
+
+- (void)updateDocument:(JDKDocument *)aDocument
+{
+    
+}
+
+- (void)deleteDocument:(JDKDocument *)aDocument
+{
+    [_internalDocuments removeObject:aDocument];
+}
+
+- (NSArray<JDKDocument *> *)documents
+{
+    return _internalDocuments.copy;
+}
 
 @end
