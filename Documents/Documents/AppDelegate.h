83b607accff490d86073238979fe45d7584907c5
John Kouris <jdkouris@gmail.com>
2020-02-04 18:19:51 -0800

    Created the skeleton Xcode project

---
+++ Documents/Documents/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Documents
+//
+//  Created by John Kouris on 2/4/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
