ed0543ae449dad52ed41b018d5121b4a2f1a843e
Robert Keffury <bobby.keffury@me.com>
2020-02-05 15:58:48 -0800

    Added wordCount file.

---
+++ Documents/Documents/NSString+WordCount.h
@@ -0,0 +1,20 @@
+//
+//  NSString+WordCount.h
+//  Documents
+//
+//  Created by Bobby Keffury on 2/5/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (WordCount)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Documents/Documents/NSString+WordCount.m
@@ -0,0 +1,16 @@
+//
+//  NSString+WordCount.m
+//  Documents
+//
+//  Created by Bobby Keffury on 2/5/20.
+//  Copyright © 2020 Bobby Keffury. All rights reserved.
+//
+
+#import "NSString+WordCount.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSString (WordCount)
+
+@end
