9f589f6e6aa16234c13c46bd8dea216197f77bb8
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-04 20:11:28 -0500

    adding Add screen

---
+++ Documents Obj-C/Documents Obj-C/Models/VVSDocument.h
@@ -0,0 +1,14 @@
+//
+//  VVSDocument.h
+//  Documents Obj-C
+//
+//  Created by Vici Shaweddy on 2/4/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VVSDocument : NSObject
+
+@end
+
---
+++ Documents Obj-C/Documents Obj-C/Models/VVSDocument.m
@@ -0,0 +1,13 @@
+//
+//  VVSDocument.m
+//  Documents Obj-C
+//
+//  Created by Vici Shaweddy on 2/4/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+#import "VVSDocument.h"
+
+@implementation VVSDocument
+
+@end
